0e54df862a8c8c8c85ea0c661a8a4f3ec2d25320
Arun37-Kumar <arun007624@gmail.com>
2022-04-11 17:23:39 +0530

    commit made

---
+++ DataStructures_LinkedList_StacksAndQueues/LinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_LinkedList_StacksAndQueues
+{
+    class LinkedList
+    {
+        // head position
+        internal Node head;
+
+        //Add the node
+        internal void AddNode(int data)
+        {
+            Node node = new Node(data);
+
+            if(head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;   
+            }
+            Console.WriteLine("{0} inserted into Linked List.", node.data);
+        }
+
+        //Display the Elements
+        public void Display()
+        {
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("Linked List is empty.");
+                return;
+            }
+            while(temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
---
+++ DataStructures_LinkedList_StacksAndQueues/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_LinkedList_StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Section !");
+
+            LinkedList list = new LinkedList();
+            list.AddNode(56);
+            list.AddNode(30);
+            list.AddNode(70);
+            Console.WriteLine("The elements in linked list are : ");
+            list.Display();
+
+            Console.ReadLine();
+        }
+    }
+}

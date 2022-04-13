d73e6aba4581f0ec99123bff43ccb9d136c684f8
Arun37-Kumar <arun007624@gmail.com>
2022-04-13 17:37:26 +0530

    commit

--- DataStructures_LinkedList_StacksAndQueues/LinkedList.cs
+++ DataStructures_LinkedList_StacksAndQueues/LinkedList.cs
@@ -118,6 +118,24 @@ namespace DataStructures_LinkedList_StacksAndQueues
             }
         }
 
+        //Search an Element
+        public bool Search(int data)
+        {
+            int index = 0;
+            Node temp = head;
+            while (temp != null)
+            {
+                index++;
+                if (temp.data.Equals(data))
+                {
+                    Console.WriteLine(data + " is Found At Index = " + index);
+                    return true;
+                }
+                temp = temp.next;
+            }
+            return false;
+        }
+
         //Inserting data into a particular position
         public void Insert(int position, int data)
         {
@@ -143,6 +161,23 @@ namespace DataStructures_LinkedList_StacksAndQueues
             }
         }
 
+        //Deleting data
+        public void Delete(int data)
+        {
+            Node temp = head;
+            Node previous = null;
+            if (temp != null && temp.data == data)
+            {
+                head = temp.next;
+            }
+            while (temp != null && temp.data != data)
+            {
+                previous = temp;
+                temp = temp.next;
+            }
+            if (temp == null)
+            previous.next = temp.next;
+        }
 
 
     }
--- DataStructures_LinkedList_StacksAndQueues/Program.cs
+++ DataStructures_LinkedList_StacksAndQueues/Program.cs
@@ -28,11 +28,12 @@ namespace DataStructures_LinkedList_StacksAndQueues
 
             //list.DeleteFirst();
             //list.deleteLastNode();
-
             list.AddNode(56);
             list.AddNode(30);
             list.AddNode(70);
             list.Insert(2,40);
+            list.Search(30);
+            list.Delete(40);
             list.Display();
             Console.ReadLine();
         }
